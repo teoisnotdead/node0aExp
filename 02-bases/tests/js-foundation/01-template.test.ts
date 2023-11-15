e07ca1e633fb6dcd363a7642c442d39c27df0ed9
TEO <alfredo.ssm@gmail.com>
2023-11-15 16:05:36 -0300

    Add test cfg to 02-bases with ts

---
+++ 02-bases/tests/js-foundation/01-template.test.ts
@@ -0,0 +1,15 @@
+import { emailTemplate } from '../../src/js-foundation/01-template';
+
+describe('js-foundation/01-template', () => {
+    test('emailTemplate should contain a greetin', () => {
+        expect(emailTemplate).toContain('Hi, ');
+    });
+
+    test('emailTemplate should contain a name', () => {
+
+      expect(emailTemplate).toContain('{{name}}');
+  });
+
+
+});
+
